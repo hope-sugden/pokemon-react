ffa92179f4ceea7db2dde80b3e16a31c3ea2defb
Stu Slicer <stu_slicer@yahoo.co.uk>
2022-04-24 15:57:26 +0100

    More added to basic layout

---
+++ src/components/PokemonImage/PokemonImage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './PokemonImage.scss';
+
+const PokemonImage = () => {
+  return (
+    <>
+      <div className='poke-image-card' >
+        PokemonImage
+      </div>
+    </>
+  )
+}
+
+export default PokemonImage
