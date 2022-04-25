43f5237ea1864c68434f80dfc92a0e493843527c
Stu Slicer <stu_slicer@yahoo.co.uk>
2022-04-25 12:37:39 +0100

    Update navbar title

--- src/containers/NavBar/NavBar.jsx
+++ src/containers/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import './NavBar.scss';
 const NavBar = () => {
   return (
     <div className='navbar' >
-         <h1 className='navbar__header' >Welcome!</h1>
+         <h1 className='navbar__header' >PokéDom - gotta code 'em all!</h1>
     </div>
   )
 }
